45eee0eee6db7b58d1f9436ca29cf488f5e28680
dj-lumiere <djminstar573@gmail.com>
2026-01-08 22:15:23 +0900

    fixed build script to download files on CI.

--- native/build.sh
+++ native/build.sh
@@ -13,13 +13,13 @@ echo "Building libraries..."
 cmake --build . --config Release
 
 echo "Copying libraries to project directories..."
-mkdir -p ../../bin/Debug/net9.0
-mkdir -p ../../bin/Release/net9.0
+mkdir -p ../../bin/Debug/net10.0
+mkdir -p ../../bin/Release/net10.0
 
 # TODO: Change it based on OS
-cp lib/*.so ../../bin/Debug/net9.0/ 2>/dev/null || true
-cp lib/*.so ../../bin/Release/net9.0/ 2>/dev/null || true
-#cp lib/*.dylib ../../bin/Debug/net9.0/ 2>/dev/null || true
-#cp lib/*.dylib ../../bin/Release/net9.0/ 2>/dev/null || true
+cp lib/*.so ../../bin/Debug/net10.0/ 2>/dev/null || true
+cp lib/*.so ../../bin/Release/net10.0/ 2>/dev/null || true
+#cp lib/*.dylib ../../bin/Debug/net10.0/ 2>/dev/null || true
+#cp lib/*.dylib ../../bin/Release/net10.0/ 2>/dev/null || true
 
 echo "Native libraries built successfully!"
